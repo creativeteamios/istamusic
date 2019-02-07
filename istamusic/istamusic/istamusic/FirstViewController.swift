a00312d2740ca312d60963f51d4a83d944fe8f40
KiritoS9 <stefanosoriano51@gmail.com>
2019-02-07 15:45:01 +0100

    Add files via upload
    Initial Commit

---
+++ istamusic/istamusic/istamusic/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  istamusic
+//
+//  Created by Soriano Stefano on 07/02/2019.
+//  Copyright © 2019 Creative Team. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ istamusic/istamusic/istamusic/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  istamusic
+//
+//  Created by Soriano Stefano on 07/02/2019.
+//  Copyright © 2019 Creative Team. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
