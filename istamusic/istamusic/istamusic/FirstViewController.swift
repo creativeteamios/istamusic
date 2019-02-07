eed86e76b189e13c0966d47d2e2c05599fac7e9a
Mariniello Umberto <marinielloumberto@san-13.local>
2019-02-07 16:21:33 +0100

    umbra

--- istamusic/istamusic/istamusic/FirstViewController.swift
+++ istamusic/istamusic/istamusic/FirstViewController.swift
@@ -17,6 +17,7 @@ class FirstViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
+        var name: String
     }
 
 
