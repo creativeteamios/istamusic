8448eeae74f20c3ea7bdd67818e3fde18bf1c6aa
Mariniello Umberto <marinielloumberto@10-25-48-135.0200.san.it.iosda.org>
2019-02-14 12:38:46 +0100

    Map Ok

---
+++ istamusic/istamusic/View/MapView.swift
@@ -0,0 +1,128 @@
+//
+//  MapView.swift
+//  istamusic
+//
+//  Created by Mariniello Umberto on 14/02/2019.
+//  Copyright © 2019 Creative Team. All rights reserved.
+//
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapView: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var myLocation: CLLocationCoordinate2D?
+    
+    let database = Database()
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        // Current position button
+        let buttonItem = MKUserTrackingBarButtonItem(mapView: mapView)
+        self.navigationItem.rightBarButtonItem = buttonItem
+        
+        if CLLocationManager.locationServicesEnabled() {
+            //locationManager.delegate = self
+            centerMap(locationManager.location!.coordinate)
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        mapView.delegate = self
+        mapView.mapType = .standard
+        mapView.isZoomEnabled = true
+        mapView.isScrollEnabled = true
+        
+        if let coor = mapView.userLocation.location?.coordinate{
+            mapView.setCenter(coor, animated: true)
+        }
+        
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
+        
+        addAnnotation()
+    }
+    
+    // Current position
+    func centerMap(_ center:CLLocationCoordinate2D){
+        let spanX = 0.700
+        let spanY = 0.700
+        
+        let newRegion = MKCoordinateRegion(center:center , span: MKCoordinateSpan(latitudeDelta: spanX, longitudeDelta: spanY))
+        mapView.setRegion(newRegion, animated: true)
+    }
+    
+    /*
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+     let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+     
+     centerMap(locValue)
+     }
+     */
+    
+    func addAnnotation() {
+        for i in 0..<database.locations.count {
+            let geoCoder = CLGeocoder()
+            
+            let annotation = MKPointAnnotation()
+            annotation.title = database.imageBand[i]
+            annotation.subtitle = database.locations[i]
+            
+            geoCoder.geocodeAddressString(annotation.subtitle!, completionHandler: { placemarks, error in
+                if let error = error {
+                    print(error)
+                    return
+                }
+                
+                if let placemarks = placemarks {
+                    let placemark = placemarks[0]
+                    
+                    if let location = placemark.location {
+                        annotation.coordinate = location.coordinate
+                        //self.mapView.showAnnotations([annotation], animated: true)
+                        //self.mapView.selectAnnotation(annotation, animated: true)
+                    }
+                }
+            })
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    // MARK: - MKMapViewDelegate methods
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyMarker"
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+        
+        // Reuse the annotation if possible
+        var annotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        
+        annotationView?.glyphText = "🎸" //command + control + space per inserire le emoticon
+        annotationView?.markerTintColor = UIColor.orange
+        
+        return annotationView
+        
+    }
+    
+    // Override MKUserTrackingBarButtonItem button
+    func mapView(_ mapView: MKMapView, didChange mode: MKUserTrackingMode, animated: Bool) {
+        centerMap(locationManager.location!.coordinate)
+    }
+    
+}
